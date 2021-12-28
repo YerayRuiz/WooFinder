56638dd48cc7ba315a09292ec54eba99047b46d8
AdriCorrales <adricorrales@uma.es>
2021-12-28 14:31:56 +0100

    He creado Animal.java

--- WooFinder/app/src/main/java/com/example/woofinder/clases/Animal.java
+++ WooFinder/app/src/main/java/com/example/woofinder/clases/Animal.java
@@ -1,4 +1,34 @@
 package com.example.woofinder.clases;
 
+import com.example.woofinder.PruebaActivity;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Animal {
+    private FirebaseFirestore db= SingletonDataBase.getInstance().get(PruebaActivity.SHARED_DATA_KEY);
+    private CollectionReference animalCollection = db.collection("Animal");
+    private Map<String, Object> animal = new HashMap<>();
+    private DocumentSnapshot doc;
+
+    public Animal(String descripcion, Timestamp fecha, GeoPoint localizacion, String tipo){
+        this.animal.put("descripcion", descripcion);
+        this.animal.put("fecha", fecha);
+        this.animal.put("localizacion", localizacion);
+        this.animal.put("tipo", tipo);
+    }
+
+    // Esto hace el update y el add
+    public void addAnimal(){
+        animalCollection.document("prueba").set(this.animal);
+    }
+    
+    public Map<String, Object> getAnimal() {
+        return animal;
+    }
 }
